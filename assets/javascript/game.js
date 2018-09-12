ea8bfd6f978aa22ab5de20033d1a474100594639
Scorpio <diallomikey13@yahoo.fr>
2018-09-12 13:18:33 -0400

    final commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+//declaring variables
+    var crystal1 = Math.floor(Math.random() * 12 + 1);
+    var crystal2 = Math.floor(Math.random() * 12 + 1);
+    var crystal3 = Math.floor(Math.random() * 12 + 1);
+    var crystal4 = Math.floor(Math.random() * 12 + 1);
+    var currentNumber = Math.floor(Math.random() * 50 + 1);
+    var wins = 0;
+    var losses = 0;
+    var totalScore = 0;
+//connecting the divs in HTML to their values in JS
+    $("#numberDisplayed").text(currentNumber);
+    $("#Wins").text(wins);
+    $("#Losses").text(losses);
+    $("#totalScore").text(totalScore);
+//Function to reset the game
+    function resetGame() {
+        currentNumber = Math.floor(Math.random() * 50 + 1);
+        console.log(currentNumber);
+        $("#numberDisplayed").text(currentNumber);
+        crystal1 = Math.floor(Math.random() * 10 + 1);
+        crystal2 = Math.floor(Math.random() * 10 + 1);
+        crystal3 = Math.floor(Math.random() * 10 + 1);
+        crystal4 = Math.floor(Math.random() * 10 + 1);
+        totalScore = 0;
+        console.log(totalScore);
+        $("#totalScore").text(totalScore);
+    }
+//Execute this function when the player wins the game    
+    function checkWins() {
+        if (totalScore == currentNumber){
+        wins++;
+        $("#Wins").text(wins);
+        $("#result").text("You won!!");
+        resetGame();
+        }
+    }
+//Execute this function when the player losses the game
+    function checkLosses() {
+        if (totalScore > currentNumber){
+            losses++;
+            $("#Losses").text(losses);
+            $("#result").text("You lost!!");
+            resetGame();
+        }
+    }
+//Events for clicking on the imges
+    $("#tall-crystal").on("click", function () {
+        totalScore += crystal1;
+        $("#totalScore").text(totalScore);
+        checkWins();
+        checkLosses();
+    });
+    $("#purple-crystal").on("click", function () {
+        totalScore += crystal2;
+        $("#totalScore").text(totalScore);
+        checkWins();
+        checkLosses();
+    });
+    $("#white-crystal").on("click", function () {
+        totalScore += crystal3;
+        $("#totalScore").text(totalScore);
+        checkWins();
+        checkLosses();
+    });
+    $("#yellow-crystal").on("click", function () {
+        totalScore += crystal4;
+        $("#totalScore").text(totalScore);
+        checkWins();
+        checkLosses();
+    });
+});
